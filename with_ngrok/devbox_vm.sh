be74ca59ed62abd8e69199049f8c92e17dd3a1ba
idk6453 <98331772+idk6453@users.noreply.github.com>
2022-02-04 07:26:38 +0700

    Update devbox_vm.sh

--- with_ngrok/devbox_vm.sh
+++ with_ngrok/devbox_vm.sh
@@ -38,7 +38,7 @@ wget -O lite11.qcow2 https://app.vagrantup.com/cvhnups/boxes/Qcow2/versions/1.0/
 
 qemu-img resize lite11.qcow2 45G
 
-nohup kvm -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga qxl -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=lite11.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
+nohup kvm -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga qxl -machine type=pc,accel=kvm -rtc base=localtime -usb -device usb-tablet -k en-us -drive file=lite11.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
 
 clear
 echo "Script by CVHNups, Windows img file by ThuongHai (https://github.com/kmille36)"
